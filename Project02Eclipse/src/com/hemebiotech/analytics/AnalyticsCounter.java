8fe792a8c3aea9e9af3e36f0eb469cbc47baccd8
Yaacov Illouz <yaacov@psm.one>
2024-03-31 00:51:34 +0100

    remove unnecessary comments

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -5,18 +5,19 @@ import java.io.FileReader;
 import java.io.FileWriter;
 
 public class AnalyticsCounter {
-	private static int headCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
+	private static int headCount = 0;
+	private static int rashCount = 0;
+	private static int dilatedPupilCount = 0;
 	
 	public static void main(String args[]) throws Exception {
-		// first get input
+
+		// read each line of file symptom.txt and count of occurences symptomes
 		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
 		String line = reader.readLine();
 
-		int i = 0;	// set i to 0
+		int i = 0;
 		while (line != null) {
-			i++;	// increment i
+			i++;
 			System.out.println("symptom from file: " + line);
 			if (line.equals("headache")) {
 				headCount++;
@@ -25,18 +26,18 @@ public class AnalyticsCounter {
 			else if (line.equals("rash")) {
 				rashCount++;
 			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
+			else if (line.equals("dialated pupils")) {
+				dilatedPupilCount++;
 			}
 
-			line = reader.readLine();	// get another symptom
+			line = reader.readLine();
 		}
 		
-		// next generate output
+		// generate a new output result.out file with list of occurences of symptomes
 		FileWriter writer = new FileWriter ("result.out");
 		writer.write("headache: " + AnalyticsCounter.headCount + "\n");
 		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
+		writer.write("dialated pupils: " + dilatedPupilCount + "\n");
 		writer.close();
 	}
 }
